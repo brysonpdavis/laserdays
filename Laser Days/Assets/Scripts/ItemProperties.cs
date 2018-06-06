faa93553be9f510f3b5865124f904f5104c60736
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-06-06 18:09:15 -0400

    objects can have charge values or boost values

--- Laser Days/Assets/Scripts/ItemProperties.cs
+++ Laser Days/Assets/Scripts/ItemProperties.cs
@@ -9,6 +9,8 @@ public class ItemProperties : MonoBehaviour
     public string itemName;
 
     [SerializeField] private bool objectCharge = true;
+    [SerializeField] private bool boost;
+
     [SerializeField] public int value;
 
     [SerializeField] private PlayerCharge playerCharge;
@@ -37,7 +39,41 @@ public class ItemProperties : MonoBehaviour
 
             }
             }
+
+
+        if (boost)
+        {
+
+            //Check to see if it can be activated
+            if (value <= playerCharge.chargeSlider.value)
+            {
+                //subtract charge value:
+                //for slider
+                playerCharge.chargeSlider.value += value;
+
+
+                playerCharge.chargeValue.text = playerCharge.chargeSlider.value.ToString();
+
+
+
+                //want something here to change the object's shader, showing that it can shift to either world
+
+
+                //turn on the switching script
+                this.GetComponent<flipScript>().enabled = true;
+
+            }
+        }
+
+
+
+
+
+
+
     }
+
+
 
 }
 
