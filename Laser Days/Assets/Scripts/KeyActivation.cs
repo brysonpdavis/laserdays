f08a1a83f327bcbd7e979ad81914a86a48415caa
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-06-15 12:04:40 -0400

    updated key activation script to turn off door on trigger exit, take array of doors

--- Laser Days/Assets/Scripts/KeyActivation.cs
+++ Laser Days/Assets/Scripts/KeyActivation.cs
@@ -2,28 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyActivation : MonoBehaviour {   
+public class KeyActivation : MonoBehaviour
+{
 
     [Header("Key Settings")]
-    [SerializeField]public string key ;
-    [SerializeField]public GameObject door;
+    [SerializeField] public string key;
+    [SerializeField] public GameObject[] door;
+
     public bool anyKey;
-  
+
     // Update is called once per frame
     void OnTriggerEnter(Collider col)
     {
+
+
         if (col.gameObject.GetComponent<ItemProperties>().isKey)
         {
             if (anyKey)
             {
-                door.SetActive(false);
+                for (int i = 0; i < door.Length; i++)
+                {
+
+
+                    door[i].SetActive(false);
+                }
             }
             else
             {
-                if(key == col.gameObject.GetComponent<ItemProperties>().itemName)
-                { door.SetActive(false); }
+                if (key == col.gameObject.GetComponent<ItemProperties>().itemName)
+
+                    for (int i = 0; i < door.Length; i++)
+                    {
+                        door[i].SetActive(false);
+                    }
+            }
+
+        }
+        }
+
+    void OnTriggerExit(Collider other)
+    {
+
+
+        for (int i = 0; i < door.Length; i++)
+        {
+
+            {
+                door[i].SetActive(true);
             }
         }
-        
     }
 }
