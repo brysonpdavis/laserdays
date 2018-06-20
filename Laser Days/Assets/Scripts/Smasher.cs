0bd8c302fa6c5e0858c381d8d209c71e368c125d
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-06-20 18:21:50 -0400

    cleanup, small comments

--- Laser Days/Assets/Scripts/Smasher.cs
+++ Laser Days/Assets/Scripts/Smasher.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Smasher : MonoBehaviour {
 
+    //bring in prefabs of the objects that you want your original object to break into
     public GameObject destroyedVersionReal;
     public GameObject destroyedVersionLaser;
 
     private void OnCollisionEnter(Collision collision)
     {
-
-        Debug.Log(collision.gameObject.layer);
-
+        //spawns fractured version of "real" object in place
         if (collision.gameObject.tag.Equals("Real"))
         {
             Instantiate(destroyedVersionReal, transform.position, transform.rotation);
             Destroy(gameObject);
         }
+        //spawns fractured version of "laser" object in place
         if (collision.gameObject.tag.Equals("Laser"))
         {
             Instantiate(destroyedVersionLaser, transform.position, transform.rotation);
