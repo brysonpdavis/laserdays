43e80f4108b59373c5ffc27de3b535fced2d7ece
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-06-20 18:18:58 -0400

    breakable object script (smasher) + 2 different kinds of fractures based on which world player is in

---
+++ Laser Days/Assets/Scripts/Smasher.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Smasher : MonoBehaviour {
+
+    public GameObject destroyedVersionReal;
+    public GameObject destroyedVersionLaser;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Debug.Log(collision.gameObject.layer);
+
+        if (collision.gameObject.tag.Equals("Real"))
+        {
+            Instantiate(destroyedVersionReal, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag.Equals("Laser"))
+        {
+            Instantiate(destroyedVersionLaser, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+    }
+
+
+}
