f3f8470ce81229dcb72827983b749675252e342f
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-06-26 21:43:59 -0400

    using gameobject loading instead of scene loading

--- Laser Days/Assets/sceneTrigger.cs
+++ Laser Days/Assets/sceneTrigger.cs
@@ -6,28 +6,28 @@ using UnityEngine.SceneManagement;
 public class sceneTrigger : MonoBehaviour
 {
 
-    public string SceneToLoad;
+    public GameObject SceneToLoad;
     public Collider linkedCollider;
     public Collider player;
-
-
 
     void OnTriggerEnter(Collider collider)
     {
         //when colliding with player, make sure target scene isn't already open, if it's not then load the scene on top (additive) of current scene
         if (collider.tag.Equals("Player"))
         {
-
-            if (!SceneManager.GetSceneByName(SceneToLoad).isLoaded)
+            
+            if (SceneToLoad.active == false)
             {
+                SceneToLoad.active = true;
 
-                SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
-
+                //old scene method:
+                //SceneManager.LoadScene(SceneToLoad, LoadSceneMode.Additive);
+           
                 //can turn off object's renderer. right now it causes a flicker, so currently off.
-                /*
-                 * MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
+
+                MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
                 m.enabled = false;
-                */
+
             }
         }
     }
@@ -38,8 +38,17 @@ public class sceneTrigger : MonoBehaviour
         //when player leaves area, make sure player's bounds are NOT within the parallel bounds before unloading the current scene
         if (collider.tag.Equals("Player"))
         {
-            if (!player.bounds.Intersects(linkedCollider.bounds))
-            {SceneManager.UnloadSceneAsync(SceneToLoad);
+            if (!player.bounds.Intersects(linkedCollider.bounds)){
+
+                SceneToLoad.active = false;
+
+                MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
+                m.enabled = true;
+
+                //old scene method
+                //{SceneManager.UnloadSceneAsync(SceneToLoad);
+
+
             }
         }
     }
