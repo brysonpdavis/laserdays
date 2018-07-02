742dc73bc715faca60536286014ad72a86e76553
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-02 12:03:27 -0400

    Added Player Spawn Point creator

---
+++ Laser Days/Assets/Scripts/PlayerBuilderEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerBuilderScript))]
+public class PlayerBuilderEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PlayerBuilderScript myScript = (PlayerBuilderScript)target;
+        if (GUILayout.Button("Build Player"))
+        {
+            myScript.BuildPlayer(myScript.spawnSelect);
+
+        }
+    }
+}
---
+++ Laser Days/Assets/Scripts/PlayerBuilderScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBuilderScript : MonoBehaviour 
+{
+    public Transform Player;
+    public Transform[] spawnPoint;
+    public int spawnSelect;
+
+
+    public void BuildPlayer(int chooser)
+    {
+
+        Player.position = spawnPoint[chooser].position;
+    }
+}
