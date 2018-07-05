5c19d1416bdf5d9305a88fb9ef940f173004f7bd
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-05 12:05:12 -0400

    Modular kit mover script added

---
+++ Laser Days/Assets/Scripts/BuildingMoverEditor.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(BuildingMoverScript))]
+public class BuildingMoverEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+
+                 
+        //GUILayout.SelectionGrid(selectedAction, actionLabels, 3, GUILayout.Width(width), GUILayout.Height(height));
+        DrawDefaultInspector();
+        BuildingMoverScript myScript = (BuildingMoverScript)target;
+
+        if (GUILayout.Button("Move Forward"))
+        {myScript.MoveForward();}
+
+        if (GUILayout.Button("Move Backward"))
+        {myScript.MoveBackward();}
+
+        if (GUILayout.Button("Move Right"))
+        { myScript.MoveRight(); }
+
+        if (GUILayout.Button("Move Left"))
+        { myScript.MoveLeft(); }
+
+        //empty slot
+        if (GUILayout.Button("--------"))
+        { }
+
+
+        if (GUILayout.Button("Rotate Clockwise"))
+        { myScript.RotateClockwise(); }
+
+
+        if (GUILayout.Button("Rotate Counter"))
+        { myScript.RotateCounter(); }
+
+        //empty slot
+        if (GUILayout.Button("--------"))
+        { }
+
+        if (GUILayout.Button("Move Up"))
+        { myScript.MoveUp(); }
+
+        if (GUILayout.Button("Move Down"))
+        { myScript.MoveDown(); }
+    }
+
+
+
+}
---
+++ Laser Days/Assets/Scripts/BuildingMoverScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingMoverScript : MonoBehaviour 
+{
+    public Vector3 move = new Vector3(9f, 4f, 5.196f);
+
+    public void MoveForward()
+    {
+        Vector3 p = this.transform.position;
+        p.z += move.z;
+        this.transform.position = p;
+
+    }
+    public void MoveBackward()
+    {
+        Vector3 p = this.transform.position;
+        p.z -= move.z;
+        this.transform.position = p;
+
+    }
+   
+    public void MoveLeft()
+    {
+        Vector3 p = this.transform.position;
+        p.x -= move.x;
+        this.transform.position = p;
+
+    }
+
+    public void MoveRight()
+    {
+        Vector3 p = this.transform.position;
+        p.x += move.x;
+        this.transform.position = p;
+    }
+
+
+    public void RotateClockwise()
+    {
+        Vector3 p = this.transform.rotation.eulerAngles;
+        p.y += 60;
+        this.transform.rotation = Quaternion.Euler(p);
+    }
+
+    public void RotateCounter()
+    {
+        Vector3 p = this.transform.rotation.eulerAngles;
+        p.y -= 60;
+
+        this.transform.rotation = Quaternion.Euler(p);
+    }
+
+    public void MoveUp()
+    {
+        Vector3 p = this.transform.position;
+        p.y += move.y;
+        this.transform.position = p;
+
+    }
+
+    public void MoveDown()
+    {
+        Vector3 p = this.transform.position;
+        p.y -= move.y;
+        this.transform.position = p;
+
+    }
+
+}
