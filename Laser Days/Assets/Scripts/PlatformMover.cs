d3ebde2df7e254ea11eddbbae5674f3af5bd022b
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-11 16:25:47 -0400

    Platform Controller added [in prefabs]

---
+++ Laser Days/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour {
+    
+
+
+    private IEnumerator MovePlatformCoroutine(Vector3 startPos, Vector3 endPos, float duration)
+    {
+        float elapsedTime = 0;
+        float ratio = elapsedTime / duration;
+        while (ratio < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            ratio = elapsedTime / duration;
+            transform.position = Vector3.Lerp(startPos, endPos, ratio);
+            yield return null;
+        }
+    }
+
+    public void MovePlatform(Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (this.transform.position == startPos)
+        {
+            StartCoroutine(MovePlatformCoroutine(startPos, endPos, duration));
+        }
+
+        else if (this.transform.position == endPos){
+            StartCoroutine(MovePlatformCoroutine(endPos, startPos, duration));
+
+        }
+    }
+
+
+
+}
---
+++ Laser Days/Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour {
+
+    public GameObject platform;
+    public Transform start;
+    public Transform end;
+    public int time = 3;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        platform.GetComponent<PlatformMover>().MovePlatform(start.position, end.position, time);
+    }
+}
