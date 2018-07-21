06fe4dc5cb5e212deb0e57434c6bc144ad648f98
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-20 21:55:09 -0400

    added booster child gameobject to sokoban

---
+++ Laser Days/Assets/Booster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Booster : MonoBehaviour {
+
+    public int boostAmount = 20;
+    GameObject player;
+
+
+    private void Start()
+    {
+        player = Toolbox.Instance.GetPlayer();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        MFPP.Modules.PickUpModule pickUp = player.GetComponent<MFPP.Modules.PickUpModule>();
+
+        if (pickUp.heldObject)
+        {
+            if (pickUp.heldObject.Equals(other.gameObject)){
+
+                pickUp.PutDown();
+            }
+        }
+
+        other.GetComponent<Rigidbody>().AddForce(transform.up * boostAmount, ForceMode.Impulse);
+        Debug.Log(transform.up * boostAmount);
+
+    }
+}
