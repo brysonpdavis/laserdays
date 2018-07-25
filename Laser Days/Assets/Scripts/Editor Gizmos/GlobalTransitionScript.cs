e9293f53ea5afde0f9b1c9e020f5630efcc2cb5c
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-25 14:14:27 -0400

    fixes

--- Laser Days/Assets/Scripts/Editor Gizmos/GlobalTransitionScript.cs
+++ Laser Days/Assets/Scripts/Editor Gizmos/GlobalTransitionScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GlobalTransitionScript : MonoBehaviour {
-    private Component[] components;
+    public Component[] components;
 
 
     public void GlobalReal()
