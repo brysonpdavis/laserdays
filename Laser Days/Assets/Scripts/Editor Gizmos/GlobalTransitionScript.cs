d4754e89716e8f4da320f94917eb11838d664304
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-07-25 13:08:21 -0400

    Revert "fixed bug in global transition script"
    This reverts commit 8cdb2f00e0d0e6462b7c600e6f1f47239f27184d.

--- Laser Days/Assets/Scripts/Editor Gizmos/GlobalTransitionScript.cs
+++ Laser Days/Assets/Scripts/Editor Gizmos/GlobalTransitionScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GlobalTransitionScript : MonoBehaviour {
-    public Component[] components;
+    private Component[] components;
 
 
     public void GlobalReal()
@@ -11,10 +11,9 @@ public class GlobalTransitionScript : MonoBehaviour {
         components = GetComponentsInChildren<Transition>();
             foreach (Transition albo in components)
             {
-            if (albo.GetComponent<Renderer>()){
-                Material material = albo.GetComponent<Renderer>().sharedMaterial;
-                material.SetFloat("_TransitionState", 0);
-            }
+            Material material = albo.GetComponent<Renderer>().sharedMaterial;
+            material.SetFloat("_TransitionState", 0);
+        
             }   
      
     }
@@ -25,65 +24,56 @@ public class GlobalTransitionScript : MonoBehaviour {
         components = GetComponentsInChildren<Transition>();
         foreach (Transition albo in components)
         {
-            if (albo.GetComponent<Renderer>())
-            {
-                
-                Material material = albo.GetComponent<Renderer>().sharedMaterial;
-                material.SetFloat("_TransitionState", 1);
-            }
+            Material material = albo.GetComponent<Renderer>().sharedMaterial;
+            material.SetFloat("_TransitionState", 1);
         }
+
     }
 
-    public void GlobalAll()
-    {
+    public void GlobalAll(){
 
         components = GetComponentsInChildren<Transition>();
         foreach (Transition albo in components)
         {
+            Material material = albo.GetComponent<Renderer>().sharedMaterial;
 
-            if (albo.GetComponent<Renderer>())
+            //set all real world objects to visible
+            if (albo.gameObject.layer == 11) {
+                material.SetFloat("_TransitionState", 0);
+            }
+
+            //set all laser world objects to visible
+            else if (albo.gameObject.layer == 10)
             {
-                Material material = albo.GetComponent<Renderer>().sharedMaterial;
-                RendererExtensions.UpdateGIMaterials(albo.GetComponent<Renderer>());
+                material.SetFloat("_TransitionState", 1);
+            }
 
-                //set all real world objects to visible
-                if (albo.gameObject.layer == 11)
-                {
-                    material.SetFloat("_TransitionState", 0);
-                }
+            //set all transition objects to halfway
+            else if (albo.gameObject.layer == 17)
+            {
+                material.SetFloat("_TransitionState", .5f);
+            }
 
-                //set all laser world objects to visible
-                else if (albo.gameObject.layer == 10)
-                {
-                    material.SetFloat("_TransitionState", 1);
-                }
-
-                //set all transition objects to halfway
-                else if (albo.gameObject.layer == 17)
-                {
-                    material.SetFloat("_TransitionState", .5f);
-                }
-
-                //SETTING LADDERS
+            //SETTING LADDERS
 
 
-                //realworld ladders
-                if (albo.gameObject.layer == 18)
-                {
-                    material.SetFloat("_TransitionState", 0);
-                }
+            //realworld ladders
+            if (albo.gameObject.layer == 18)
+            {
+                material.SetFloat("_TransitionState", 0);
+            }
 
-                //laserworld ladders
-                if (albo.gameObject.layer == 19)
-                {
-                    material.SetFloat("_TransitionState", 1);
-                }
-
-
+            //laserworld ladders
+            if (albo.gameObject.layer == 19)
+            {
+                material.SetFloat("_TransitionState", 1);
             }
 
 
         }
 
+
     }
+
+
 }
