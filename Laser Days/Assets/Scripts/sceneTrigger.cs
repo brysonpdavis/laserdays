6ceaa0c8466b248e3f87b762632c8005b9250afa
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-08-21 13:34:49 -0400

    new multiscene after dealing with idiotic github errors

--- Laser Days/Assets/Scripts/sceneTrigger.cs
+++ Laser Days/Assets/Scripts/sceneTrigger.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,8 +8,15 @@ public class sceneTrigger : MonoBehaviour
 
     public GameObject SceneToLoad;
     public GameObject backgroundScene;
-    public Collider linkedCollider;
-    public Collider player;
+    private Collider linkedCollider;
+    private Collider player;
+
+    private void Start()
+    {
+        player = Toolbox.Instance.GetPlayer().GetComponent<CharacterController>();
+        linkedCollider = GetComponent<BoxCollider>();
+        backgroundScene = GameObject.FindWithTag("BackgroundLevel");
+    }
 
     void OnTriggerEnter(Collider collider)
     {
@@ -19,15 +26,16 @@ public class sceneTrigger : MonoBehaviour
 
             if (SceneToLoad.active == false)
             {
-                SceneToLoad.active = true;
+                SceneToLoad.SetActive(true);
 
 
                 //adds any held object to the scene that the player is entering
-                if (player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject != null) {
+                if (player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject != null)
+                {
 
-                GameObject held = player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject;
-                held.transform.parent = SceneToLoad.transform;
-                 }
+                    GameObject held = player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject;
+                    held.transform.parent = SceneToLoad.transform;
+                }
 
                 //can turn off object's renderer
                 //MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
@@ -41,29 +49,25 @@ public class sceneTrigger : MonoBehaviour
 
     void OnTriggerExit(Collider collider)
     {
-        if (collider.tag.Equals("Player"))
+        if (collider.gameObject.Equals(player.gameObject))
         {
-            //when player leaves area, make sure player's bounds are NOT within the 
-            //parallel bounds before unloading the current scene
-            if (!player.bounds.Intersects(linkedCollider.bounds)){
 
-                //if player is holding an object while leaving a scene it'll be put in the general scene, unless player brings it into to another scene
+            //if player is holding an object while leaving a scene it'll be put in the general scene, unless player brings it into to another scene
 
-                if (player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject != null)
-                {
-                    GameObject held = player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject;
-                    held.transform.parent = backgroundScene.transform;
-                }
-                SceneToLoad.active = false;
-
-                MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
-                m.enabled = true;
-
-                //old scene method
-                //{SceneManager.UnloadSceneAsync(SceneToLoad);
-
-
+            if (player.GetComponent<MFPP.Modules.PickUpModule>().heldObject)
+            {
+                player.GetComponent<MFPP.Modules.PickUpModule>().PutDown();
+                // GameObject held = player.GetComponentInParent<MFPP.Modules.PickUpModule>().heldObject;
+                // held.transform.parent = backgroundScene.transform;
             }
+            SceneToLoad.SetActive(false);
+
+            // MeshRenderer m = this.GetComponentInParent<MeshRenderer>();
+            // m.enabled = true;
+
+            //old scene method
+            //{SceneManager.UnloadSceneAsync(SceneToLoad);
+
         }
     }
 
