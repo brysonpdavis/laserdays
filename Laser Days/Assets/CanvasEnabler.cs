bb8fa01254e13c8ba7aa6ac6f7fce032f305c3d8
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-08-22 15:29:39 -0400

    added canvas enabler

---
+++ Laser Days/Assets/CanvasEnabler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasEnabler : MonoBehaviour {
+    public GameObject canvas;
+	// Use this for initialization
+	void Awake () {
+        canvas.SetActive(true);
+    }
+
+}
