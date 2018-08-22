9dfa78c1b0ea3119cb7de773a60daac04db4951d
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-08-22 08:25:09 -0400

    deleted duplicate raycast manager and playercharge scripts on player
    tweaked scripts to be sure that multiples can't be added in the future

--- Laser Days/Assets/Scripts/RaycastManager.cs
+++ Laser Days/Assets/Scripts/RaycastManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[DisallowMultipleComponent]
+
+
 public class RaycastManager : MonoBehaviour {
 
     public GameObject raycastedObj;
