346e4714e9b207da60c7b476ded3783bad660f66
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-09-12 10:47:17 -0400

    light rig

---
+++ Laser Days/Assets/LightTransition.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTransition : MonoBehaviour {
+
+    private IEnumerator flipTransition;
+    public float LaserIntensity;
+    public Color LaserColor;
+    public float RealIntensity;
+    public Color realColor;
+    private Light light;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        GameObject Player = Toolbox.Instance.GetPlayer();
+
+        if (Player.layer == 15)
+        {
+            light.intensity = LaserIntensity;
+            light.color = LaserColor;
+        }
+        else if (Player.layer == 16)
+            light.intensity = RealIntensity;
+            light.color = realColor;
+    }
+
+
+    public void Flip(bool direction, float duration)
+    {
+        if (direction) {
+            flipTransition = flipTransitionRoutine(RealIntensity, realColor,  duration);
+        }
+
+        else 
+            flipTransition = flipTransitionRoutine(LaserIntensity, LaserColor, duration);
+
+        StopAllCoroutines();
+        StartCoroutine(flipTransition);
+
+    }
+
+    private IEnumerator flipTransitionRoutine(float endpoint, Color endColor, float duration)
+    {
+
+        float elapsedTime = 0;
+        float actualDuration = duration*(1-(light.intensity / endpoint));
+        float ratio = elapsedTime / actualDuration;
+        //int property = Shader.PropertyToID("_D7A8CF01");
+
+        while (ratio < 1f)
+        {
+            elapsedTime += Time.deltaTime;
+            ratio = elapsedTime / duration;
+            float value = Mathf.Lerp(light.intensity, endpoint, ratio);
+            light.intensity = value;
+
+            Color colorValue = Color.Lerp(light.color, endColor, ratio);
+            light.color = colorValue;
+
+            yield return null;
+        }
+    }
+
+}
---
+++ Laser Days/Assets/LightTransitionController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTransitionController : MonoBehaviour {
+
+    private PlayerCharge pc;
+    private GameObject player;
+    public float speed = .4f;
+    private Component[] components;
+
+    // Use this for initialization
+    void Start()
+    {
+        //automatically set up the player and charge that script will be checking
+
+        player = Toolbox.Instance.GetPlayer();
+        pc = player.GetComponent<PlayerCharge>();
+        components = GetComponentsInChildren<LightTransition>();
+    }
+
+    void LateUpdate() //check to make sure the switch is possible (just as player is doing at the same time)
+    {
+
+
+        bool isFlipped = player.GetComponent<flipScript>().flippedThisFrame;
+        bool direction = player.GetComponent<flipScript>().space;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (isFlipped)
+            {
+                FlipSurrounding(direction);
+            }
+
+        }
+
+        isFlipped = false;
+    }
+
+    void FlipSurrounding(bool direction)
+    {
+
+
+        if (direction) //means player has switched to 'real world': material should transition from current value to zero
+        {
+
+            foreach (LightTransition albo in components)
+            {
+                albo.Flip(true, speed);
+
+            }
+        }
+        else
+        {
+            foreach (LightTransition albo in components)
+            {
+                albo.Flip(false, speed);
+
+            }
+        }   
+    }
+}
