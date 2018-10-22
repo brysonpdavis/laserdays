d9c37e020242e5883ddb24c3c691fa246753fcc5
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-10-22 18:16:31 -0400

    added text narration obj/prefab

---
+++ Laser Days/Assets/Scripts/TextNarration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextNarration : MonoBehaviour {
+
+    public string text;
+    private Text canvasText;
+
+
+	// Use this for initialization
+	void Start () {
+        canvasText = GameObject.Find("TextNarration").GetComponent<Text>();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvasText.text = text;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvasText.text = null;
+        }
+    }
+
+}
