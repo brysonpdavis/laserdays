c7223d08670db9770f86cbff214822c778af696d
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-10-26 09:35:02 -0400

    Unity can't fuck us!!! (hit cmd-g to enter/exit play mode from now on)

---
+++ Laser Days/Assets/Scripts/MFPP_Scripts/Editor/DeselectMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+public class DeselectMenu : MonoBehaviour
+{
+    // Validated menu item.
+    // Add a menu item named "Log Selected Transform Name" to MyMenu in the menu bar.
+    // We use a second function to validate the menu item
+    // so it will only be enabled if we have a transform selected.
+
+    // Add a menu item named "Do Something with a Shortcut Key" to MyMenu in the menu bar
+    // and give it a shortcut (ctrl-g on Windows, cmd-g on macOS).
+    [MenuItem("DeselectPlay/Deselect All and Play %g")]
+    static void DoSomethingWithAShortcutKey()
+    {
+        Debug.Log("Deselecting and Starting");
+        Selection.objects = new UnityEngine.Object[0];
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+}
