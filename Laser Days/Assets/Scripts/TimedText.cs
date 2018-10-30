3b1b64f897df58039d4e78754ea247d6c741e18a
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-10-30 18:47:35 -0400

    timedText prefab

---
+++ Laser Days/Assets/Scripts/TimedText.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimedText : MonoBehaviour {
+
+    public string text;
+    public float waitMinutes = 2;
+    public float durationSeconds = 10f;
+    private Text canvasText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        canvasText = GameObject.Find("TextNarration").GetComponent<Text>();
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(WaitForText());
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canvasText.text = null;
+        StopAllCoroutines();  
+    }
+
+    private IEnumerator WaitForText()
+    {
+        yield return new WaitForSeconds(waitMinutes*60f);
+        canvasText.text = text;
+        yield return new WaitForSeconds(durationSeconds);
+        canvasText.text = null;
+
+    }
+
+
+
+}
