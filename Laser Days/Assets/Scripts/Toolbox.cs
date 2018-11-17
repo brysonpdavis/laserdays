52876503619bca367d18d1ef1beed94aee351478
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-11-16 21:11:40 -0500

    toolbox refix

--- Laser Days/Assets/Scripts/Toolbox.cs
+++ Laser Days/Assets/Scripts/Toolbox.cs
@@ -31,16 +31,6 @@ public class Toolbox : Singleton<Toolbox>
     public IList<Material> sharedMaterials;
     public MFPP.SoundContainerAsset testercubeSounds;
 
-    //plant materials
-    public Material realFoliage;
-    public Material[] realFoliageArray;
-
-    public Material laserFoliage;
-    public Material[] laserFoliageArray;
-
-
-
-
     void Awake()
     {
 
@@ -53,12 +43,6 @@ public class Toolbox : Singleton<Toolbox>
 
         soundEffectsSlider = pauseMenu.transform.GetChild(2).GetComponent<Slider>();
         soundEffectsSlider.onValueChanged.AddListener(delegate { VolumeChangeCheck(); });
-
-
-        //foliage instantiation
-        FoliageSetup(laserFoliageArray, laserFoliage);
-        FoliageSetup(realFoliageArray, realFoliage);
-
     }
 
     void VolumeChangeCheck()
@@ -154,15 +138,6 @@ public class Toolbox : Singleton<Toolbox>
         return pauseMenu;
     }
 
-    public Material GetLaserFoliageMaterial()
-    {
-        return laserFoliageArray[Random.Range(0, laserFoliageArray.Length)];
-    }
-
-    public Material GetRealFoliageMaterial()
-    {
-        return realFoliageArray[Random.Range(0, realFoliageArray.Length)];
-    }
 
     public void UpdateToolbox()
     {
@@ -230,25 +205,6 @@ public class Toolbox : Singleton<Toolbox>
             Toolbox.Instance.laserCore = laserCore;
             Toolbox.Instance.pauseMenu = pauseMenu;
             Toolbox.Instance.testercubeSounds = testercubeSounds;
-            Toolbox.Instance.laserFoliageArray = laserFoliageArray;
-            Toolbox.Instance.laserFoliage = laserFoliage;
-            Toolbox.Instance.realFoliageArray = realFoliageArray;
-            Toolbox.Instance.realFoliage = realFoliage;
-        }
-    }
-
-    void FoliageSetup(Material[] materialArray, Material material)
-    {
-        materialArray = new Material[9];
-        for (int x = 0; x < 3; x++)
-        {
-            for (int y = 0; y < 3; y++)
-            {
-                Material temp = material;
-                temp.SetFloat("_colShiftX", .16f+(x *.33f));
-                temp.SetFloat("_colShiftY", .16f+(y * .33f));
-                materialArray[y * 3 + x] = temp;
-            }
         }
     }
 
