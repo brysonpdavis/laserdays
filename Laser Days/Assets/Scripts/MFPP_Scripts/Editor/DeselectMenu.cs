7ef79a1cdad561b4c740001ba5a92a81e57ab2fb
Will Freudenheim <wfreudenheim@wesleyan.edu>
2018-11-28 12:57:54 -0500

    copy and paste materials between objects

--- Laser Days/Assets/Scripts/MFPP_Scripts/Editor/DeselectMenu.cs
+++ Laser Days/Assets/Scripts/MFPP_Scripts/Editor/DeselectMenu.cs
@@ -9,12 +9,33 @@ public class DeselectMenu : MonoBehaviour
 
     // Add a menu item named "Do Something with a Shortcut Key" to MyMenu in the menu bar
     // and give it a shortcut (ctrl-g on Windows, cmd-g on macOS).
+    static Material myMaterial;
+
     [MenuItem("RustForms/Deselect All and Play %g")]
     static void DoSomethingWithAShortcutKey()
     {
         Debug.Log("Deselecting and Starting");
         Selection.objects = new UnityEngine.Object[0];
         EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+
+
+    [MenuItem("RustForms/Copy Material &w")]
+    static void CopyMaterial()
+    {
+        GameObject current = (GameObject)Selection.activeObject;
+
+        if (current.GetComponent<Renderer>().sharedMaterial)
+            myMaterial = current.GetComponent<Renderer>().sharedMaterial;
+    }
+
+    [MenuItem("RustForms/Paste Material %w")]
+    static void PasteMaterial()
+    {
+        GameObject current = (GameObject)Selection.activeObject;
+        if (myMaterial && current.GetComponent<Renderer>().sharedMaterial)
+            current.GetComponent<Renderer>().sharedMaterial = myMaterial;
+           
     }
 
 }
