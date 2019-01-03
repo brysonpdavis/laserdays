1fb1c70f20f9b20f8317044ce65d913b1121be09
Will Freudenheim <wfreudenheim@wesleyan.edu>
2019-01-03 14:24:46 -0500

    soundtrack zone prefab added

---
+++ Laser Days/Assets/SoundtrackZone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundtrackZone : MonoBehaviour {
+
+    public MFPP.FlipClipAsset mySoundtrack;
+
+    GameObject player;
+    SoundTrackManager soundtrack;
+
+	// Use this for initialization
+	void Start () {
+        player = Toolbox.Instance.GetPlayer();
+        soundtrack = player.GetComponentInChildren<SoundTrackManager>();
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(player))
+        {
+            soundtrack.flipClip = mySoundtrack;
+        }
+    }
+}
