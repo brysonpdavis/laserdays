3ef4850462b773332c608ea922f9dfc83ff06830
Will Freudenheim <wfreudenheim@wesleyan.edu>
2019-01-30 12:54:37 -0500

    tweaks for screenshots

---
+++ Laser Days/Assets/CanvasOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasOff : MonoBehaviour {
+
+    public GameObject canvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            if (canvas.activeInHierarchy)
+                canvas.SetActive(false);
+            else
+                canvas.SetActive(true);
+        }
+	}
+}
