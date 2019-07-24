c5c2c1698860307fd262e0802331563ed7e2fb1f
Will Freudenheim <wfreudenheim@wesleyan.edu>
2019-07-24 16:09:11 -0400

    sounds and sound prefabs

--- Laser Days/Assets/Scripts/TriggeredAudio.cs
+++ Laser Days/Assets/Scripts/TriggeredAudio.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TriggeredAudio : MonoBehaviour {
 
     AudioSource audio;
+    public AudioSource whisperTone;
 
     private void Awake()
     {
@@ -19,7 +20,28 @@ public class TriggeredAudio : MonoBehaviour {
             audio.maxDistance = 500;
             audio.spatialBlend = 0f;
             audio.rolloffMode = AudioRolloffMode.Custom;
+
+            StartCoroutine(VolumeFade(whisperTone, 0f, 1f, 0f));
+            StartCoroutine(VolumeFade(audio, 1f, 3f, 1f));
         }
+    }
+
+    IEnumerator VolumeFade(AudioSource _AudioSource, float _EndVolume, float _FadeLength, float waitTime)
+    {
+
+        float _StartVolume = _AudioSource.volume;
+        float _StartTime = Time.time;
+
+        while (Time.time < (_StartTime + _FadeLength))
+        {
+
+            _AudioSource.volume = _StartVolume + ((_EndVolume - _StartVolume) * ((Time.time - _StartTime) / _FadeLength));
+            Debug.Log("FADE " + _AudioSource.volume);
+            yield return null;
+        }
+
+        if (_EndVolume == 0f) { _AudioSource.Stop(); }
+
     }
 
 
