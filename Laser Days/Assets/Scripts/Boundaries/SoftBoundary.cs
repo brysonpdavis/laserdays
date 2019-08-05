bed6b58d22c6e4e676d5a64986966706b0140d48
Bryson <brysonpd@gmail.com>
2019-08-05 16:03:54 -0400

    redux

--- Laser Days/Assets/Scripts/Boundaries/SoftBoundary.cs
+++ Laser Days/Assets/Scripts/Boundaries/SoftBoundary.cs
@@ -1,7 +1,25 @@
-namespace Boundaries
+using System;
+using UnityEngine;
+
+public class SoftBoundary : MonoBehaviour
 {
-    public class SoftBoundary
+    [SerializeField]
+    private float speedMultiplier;
+
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            MFPP.Player.SetBoundaryMultiplier(speedMultiplier);
+        }    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MFPP.Player.SetBoundaryMultiplier(1f);
+        }
     }
 }
