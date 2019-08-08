69a99eeaaadd80b49a099aedf7c774d4c9946dcc
Seamus Edson <sedson@wesleyan.edu>
2019-08-08 11:10:21 -0400

    Dialog bubble, Compound tweaks, and smooth stairs

---
+++ Laser Days/Assets/UIElementBob.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UIElementBob : MonoBehaviour {
+
+    public float bobMag;
+    public float timeScale;
+    private RectTransform rect;
+    private float ogPos;
+
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        ogPos = rect.transform.position.y;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 t = rect.transform.position;
+        rect.transform.position = new Vector3(t.x, ogPos + Mathf.Sin(Time.time * timeScale) * bobMag, t.z);
+    }
+
+}
