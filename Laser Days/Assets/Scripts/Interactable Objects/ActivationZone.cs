4c1037670f03a714e57d4306e3f16dcb1bd8092c
brysonpdavis <34175776+brysonpdavis@users.noreply.github.com>
2019-09-04 22:35:20 -0400

    destroy challenge 1 - arrow activates when fall

---
+++ Laser Days/Assets/Scripts/Interactable Objects/ActivationZone.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationZone : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] objectsToActivate;
+    private List<IActivatable> interfacesToActivate;
+
+	void Start () {
+        interfacesToActivate = new List<IActivatable>();
+
+		foreach (GameObject obj in objectsToActivate)
+        {
+            foreach (IActivatable component in obj.GetComponents<IActivatable>())
+            {
+                interfacesToActivate.Add(component);
+            }
+        }
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            foreach(IActivatable component in interfacesToActivate)
+            {
+                component.Activate();
+            }
+        }
+    }
+}
